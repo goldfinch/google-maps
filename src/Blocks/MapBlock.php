a5f24041b32874629a32b96a6625236b4e97f63f
Art Karasev <as.karasyov@gmail.com>
2023-12-14 10:44:52 +1300

    icon tweak

--- src/Blocks/MapBlock.php
+++ src/Blocks/MapBlock.php
@@ -16,7 +16,7 @@ class MapBlock extends BaseElement
 
     private static $inline_editable = false;
     private static $description = '';
-    private static $icon = 'bi-geo-alt-fill';
+    private static $icon = 'font-icon-p-map';
     // private static $disable_pretty_anchor_name = false;
     // private static $displays_title_in_template = true;
 
