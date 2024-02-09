8591e267556d75edd9d74e43857841e5c7d42acf
Art Karasev <goldfinch@users.noreply.github.com>
2024-02-10 10:39:36 +1300

    WIP

--- src/Blocks/MapBlock.php
+++ src/Blocks/MapBlock.php
@@ -34,16 +34,4 @@ class MapBlock extends BaseElement
 
         return $fields;
     }
-
-    public function getSummary()
-    {
-        return $this->getDescription();
-    }
-
-    public function getType()
-    {
-        $default = $this->i18n_singular_name() ?: 'Block';
-
-        return _t(__CLASS__ . '.BlockType', $default);
-    }
 }
